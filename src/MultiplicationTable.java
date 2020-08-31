87f85e250f463ab3bbb713a5c923426489d1662d
Aizik_Anonymous <isaac.ogunleye@hotmail.com>
2020-08-31 18:54:07 +0100

    Multiplication Table Problem

---
+++ src/MultiplicationTable.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MultiplicationTable {
+
+    private static ArrayList<ArrayList<Integer>> mainTable = new ArrayList<>();
+
+    public static ArrayList PrintMultiplicationTable(int max_limit, int height){
+        ArrayList<Integer> table = new ArrayList<>();
+        int multiplier = 1;
+
+        for(int i =1; i <= height; i++ ){
+            for (int j = 1; j <= max_limit; j++){
+                table.add(i * multiplier);
+                multiplier++;
+            }
+            multiplier = 1; // set multiplier back to 1
+        }
+        mainTable.add(table);
+
+        return mainTable;
+    }
+
+    public static int[][] PrintMultiplicationTable2(int max_limit, int height){
+        int[][] table = new int[height][max_limit];
+        int multiplier = 1;
+
+        for(int i =1; i <= height; i++ ){
+            for (int j = 1; j <= max_limit; j++){
+                table[i - 1][j - 1 ] = (i * multiplier);
+                multiplier++;
+            }
+            multiplier = 1; // set multiplier back to 1
+        }
+
+        return table;
+    }
+
+    public static void main(String[] args) {
+        int[][] myArray = PrintMultiplicationTable2(4,10);
+//        for(int[] arr : myArray)
+//            System.out.println(Arrays.toString(arr));
+
+        for (int i = 0; i < myArray.length; i++){
+            for (int j = 0; j < myArray[i].length; j++){
+                System.out.print(myArray[i][j] + "\t\t\t\t\t");
+            }
+            System.out.println();
+        }
+    }
+}
